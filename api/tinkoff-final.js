550516262741ca06464005820d2feef6f7236a69
Мария <maria@MacBook-Pro-MS.local>
2025-12-26 20:20:27 +0300

    Изменен taxation на 'usn' в tinkoff-final.js

--- api/tinkoff-final.js
+++ api/tinkoff-final.js
@@ -82,7 +82,7 @@ export default async function handler(req, res) {
         Email: email || 'test@example.com',
         Phone: phone || '+79991234567',
         EmailCompany: 'sokovdima3@gmail.com',
-        TaxationSystem: 'osn',
+        TaxationSystem: 'usn',
         Items: [{
           Name: cleanDescription.substring(0, 128),
           Price: Math.round((amount || 1000) * 100),
@@ -212,7 +212,7 @@ export default async function handler(req, res) {
       Email: email,
       Phone: phone,
       EmailCompany: 'sokovdima3@gmail.com',
-      TaxationSystem: 'osn', // УСН Доход
+      TaxationSystem: 'usn', // УСН Доход
       Items: [
         {
           Name: cleanDescription.substring(0, 128), // Название экскурсии
